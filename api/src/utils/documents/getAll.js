a19f27d47e2acc3cc86c222d24958b58ea99552a
Thales Gabriel <thalesg88@gmail.com>
2021-05-17 10:36:34 -0300

    fix: :bug: pupeeter was not working in docker container

--- api/src/utils/documents/getAll.js
+++ api/src/utils/documents/getAll.js
@@ -5,16 +5,27 @@ const getAll = async (url) => {
   console.log(pptUrl);
 
   try {
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({
+      executablePath: 'google-chrome',
+      args: [
+          '--no-sandbox',
+          '--disable-setuid-sandbox',
+          '--disable-dev-shm-usage',
+          '--disable-accelerated-2d-canvas',
+          '--disable-gpu',
+          '--window-size=1920x1080',
+      ],
+  });
     const page = await browser.newPage();
 
     await page.goto(pptUrl);
 
+    // This means that when the label "No results" gets invisible on the screen it is time to fetch our html
     await page.waitFor(
       () => document.querySelector("h3.mb-1").offsetParent === null
     );
 
-    // Wait for the results page to load and display the results.
+    // This will fetch all the elements that have the classes in your set class. In this case all links that represent file.
     const resultsSelector =
       ".js-tree-browser-result-anchor.tree-browser-result.no-underline.p-1.py-2.color-bg-secondary.border-bottom.d-block";
     await page.waitForSelector(resultsSelector);
